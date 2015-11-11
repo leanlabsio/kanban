cca89b5bfb1160214103eb2449321e4ffa556dfe
cnam812 <cnam812@gmail.com>
2015-11-11 19:06:32 +0300

    resolve bug from issue #91 with dots in project name

--- modules/gitlab/project.go
+++ modules/gitlab/project.go
@@ -72,7 +72,7 @@ func (g *GitlabContext) ListProjects(o *ProjectListOptions) ([]*Project, error) 
 // GitLab API docs:
 // http://doc.gitlab.com/ce/api/projects.html#get-single-project
 func (g *GitlabContext) ItemProject(project_id string) (*Project, error) {
-	path := getUrl([]string{"projects", strings.Replace(url.QueryEscape(project_id), ".", "%2E", -1)}))
+	path := getUrl([]string{"projects", strings.Replace(url.QueryEscape(project_id), ".", "%2E", -1)})
 	req, _ := http.NewRequest("GET", path, nil)
 
 	var ret Project
